8d2bbc748b1ba1783d5bfb98b34a99d9517c6cfe
Hữu Tấn <tan.nguyen2@onemount.com>
2021-09-17 01:20:57 +0700

    abcd

---
+++ baitapbuoi3/container/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
+)
+
+type SportContainer []types.Port
+
+func (listPorts SportContainer) ListPort() string {
+	if listPorts != nil {
+		resultPort := []string{}
+		for _, items := range listPorts {
+			if items.PublicPort != 0 {
+				resultPort = append(resultPort, fmt.Sprintf("%v:%v->%v/%v ", items.IP, items.PublicPort, items.PrivatePort, items.Type))
+			} else {
+				resultPort = append(resultPort, fmt.Sprintf("%v/%v ", items.PrivatePort, items.Type))
+			}
+		}
+		return strings.Join(resultPort, " ")
+	}
+	return ""
+}
+
+func main() {
+	ctx := context.Background()
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		panic(err)
+	}
+
+	containers, err := cli.ContainerList(ctx, types.ContainerListOptions{All: true})
+	if err != nil {
+		panic(err)
+	}
+
+	for _, container := range containers {
+		listPorts := SportContainer(container.Ports)
+		fmt.Printf("%s %s %s %s\n", container.ID[:10], container.Image, listPorts.ListPort(), container.Status)
+	}
+}
